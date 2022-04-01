1d4e76674949a4d54febc1e74e236961a585a8cc
ISAAC DESMOND NRUEM <isaac1436@student.babcock.edu.ng>
2022-04-01 09:04:36 +0100

    Update

---
+++ Exercise5/Program.cs
@@ -0,0 +1,26 @@
+﻿class Exercise5
+{
+    static void Main()
+    {
+        bool errorcatch = false;
+        double a=0, b=0,hyp;
+        Console.WriteLine(@" |\ ");
+        Console.WriteLine(@" | \ ");
+        Console.WriteLine(@"a|  \ ");
+        Console.WriteLine(@" |   \ ");
+        Console.WriteLine(@" |    \ ");
+        Console.WriteLine(@" |_____\");
+        Console.WriteLine("     b    ");
+
+        Console.Write("\nPlease enter the value of side \'a\': ");
+        while (errorcatch == false) { errorcatch = double.TryParse(Console.ReadLine(), out a); }
+
+        errorcatch = false;
+        Console.Write("\nPlease enter the value of side \'b\': ");
+        while (errorcatch == false) { errorcatch = double.TryParse(Console.ReadLine(), out b); }
+
+        hyp = Math.Sqrt(Math.Pow(a,2)+ Math.Pow(b , 2));
+
+        Console.WriteLine("\nThe value of the hypotenuse of the right angle triangle is {0}",hyp);
+    }
+}
